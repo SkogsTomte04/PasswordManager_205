99d63e8fb97decb64420033a16a677442488c645
William Möller <williammoller321@gmail.com>
2025-06-12 13:50:11 +1200

    Database Added

---
+++ WpfApp1/Database/SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿
+CREATE TABLE Users (
+    UserId INT PRIMARY KEY IDENTITY(1,1),
+    MasterUsername NVARCHAR(100) NOT NULL,
+    MasterPasswordHash NVARCHAR(256) NOT NULL -- Hashed master password
+);
+
